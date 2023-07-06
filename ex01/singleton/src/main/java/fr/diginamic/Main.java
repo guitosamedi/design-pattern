55691f72178d3e3d19c7ef29c9c8cd07d891885f
G33k0 <samedig@gmail.coom>
2023-07-06 10:54:19 +0200

    ex1

---
+++ ex01/singleton/src/main/java/fr/diginamic/Main.java
@@ -0,0 +1,20 @@
+package fr.diginamic;
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+            // Singleton singleton = new Singleton(); constructeur private instance impossible
+            Singleton singleton = Singleton.getInstance();
+            //singleton.getMessage();
+            System.out.println("Singleton : " + singleton);
+            singleton.setMessage(" Message : " + singleton.getMessage());
+
+            try {
+                Singleton singleton2 = singleton.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+            //System.out.println("Singleton2 : " + singleton2.clone());
+            //singleton.setMessage(" Message2 : " + singleton2.getMessage());
+           //System.out.println(singleton);
+        }
+}
