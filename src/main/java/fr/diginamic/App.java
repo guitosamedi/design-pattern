3666efc2ea53e226d0e192d88beef7b6b7c2928d
G33k0 <samedig@gmail.coom>
2023-07-06 16:19:19 +0200

    singleton ex01 branche

--- src/main/java/fr/diginamic/App.java
+++ src/main/java/fr/diginamic/App.java
@@ -1,7 +1,22 @@
 package fr.diginamic;
 
+import fr.diginamic.singleton.Singleton;
+
 public class App {
     public static void main(String[] args) throws CloneNotSupportedException {
-        System.out.println("App");
+        // Singleton singleton = new Singleton(); constructeur private instance impossible
+        Singleton singleton = Singleton.getInstance();
+        //singleton.getMessage();
+        System.out.println("Singleton : " + singleton);
+        singleton.setMessage(" Message : " + singleton.getMessage());
+
+        try {
+            Singleton singleton2 = singleton.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+        //System.out.println("Singleton2 : " + singleton2.clone());
+        //singleton.setMessage(" Message2 : " + singleton2.getMessage());
+        //System.out.println(singleton);
         }
 }
